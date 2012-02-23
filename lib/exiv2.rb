0ee4b0cb994d9755c44d482846048e9e261f9448
Steve Hodgkiss <steve@hodgkiss.me>
2012-02-24 09:16:03 +1100

    UTF-8 encoding comments

--- lib/exiv2.rb
+++ lib/exiv2.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'exiv2/exiv2'
 require 'exiv2/shared_methods'
 require 'exiv2/exif_data'
--- lib/exiv2/xmp_data.rb
+++ lib/exiv2/xmp_data.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class Exiv2::XmpData
   include SharedMethods
 end
