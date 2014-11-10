fde04438ef4ad6ff0cec4ee0f5ccff327433a00c
Jan Jedrychowski <jan@format.com>
2014-11-10 11:09:39 +0100

    Log error if exiv2 devel package is missing

--- ext/exiv2/extconf.rb
+++ ext/exiv2/extconf.rb
@@ -1,4 +1,8 @@
 require 'mkmf'
 config = pkg_config('exiv2')
-$CPPFLAGS = [$CPPFLAGS, config[0]].join ' '
-create_makefile("exiv2/exiv2")
+if config
+  $CPPFLAGS = [$CPPFLAGS, config[0]].join ' '
+  create_makefile("exiv2/exiv2")
+else
+  abort 'exiv2 devel package is missing'
+end
