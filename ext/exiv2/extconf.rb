9bfaae2597ce2146b81a36c8439b9e0304b4f2f4
Jan Jedrychowski <jan@format.com>
2016-07-25 15:21:41 +0200

    Add default pkg-config to extconf.rb

--- ext/exiv2/extconf.rb
+++ ext/exiv2/extconf.rb
@@ -5,6 +5,7 @@ RbConfig::MAKEFILE_CONFIG['CCFLAGS'] = ENV['CCFLAGS'] if ENV['CCFLAGS']
 RbConfig::MAKEFILE_CONFIG['CXX'] = ENV['CXX'] if ENV['CXX']
 RbConfig::MAKEFILE_CONFIG['CXXFLAGS'] = ENV['CXXFLAGS'] if ENV['CXXFLAGS']
 
+pkg_config("exiv2")
 dir_config("exiv2")
 have_library("exiv2")
 create_makefile("exiv2/exiv2")
