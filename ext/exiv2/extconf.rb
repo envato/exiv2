e9c1bb62c9f711cb5f53a181329c9305d07b7b25
Stanisław Pitucha <stan.pitucha@envato.com>
2022-02-01 17:11:16 +1100

    Detect the exiv library using pkg-config
    This should get rid of most problems with library location detection
    without the need for manually supplying the path.
    Previous ways of providing the specific path are still available.

--- ext/exiv2/extconf.rb
+++ ext/exiv2/extconf.rb
@@ -5,6 +5,8 @@ RbConfig::MAKEFILE_CONFIG['CCFLAGS'] = ENV['CCFLAGS'] if ENV['CCFLAGS']
 RbConfig::MAKEFILE_CONFIG['CXX'] = ENV['CXX'] if ENV['CXX']
 RbConfig::MAKEFILE_CONFIG['CXXFLAGS'] = ENV['CXXFLAGS'] if ENV['CXXFLAGS']
 
-dir_config("exiv2")
+if dir_config("exiv2") == [nil, nil]
+  pkg_config("exiv2")
+end
 have_library("exiv2")
 create_makefile("exiv2/exiv2")
