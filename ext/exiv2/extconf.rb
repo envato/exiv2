17e587a7d95ac5cad53156cc08dc209fe637c2c8
Stanisław Pitucha <stan.pitucha@envato.com>
2024-01-08 11:05:36 +1100

    Update required CXXFLAGS (c++ standard)
    Also remove the unused / not working flags from MAKEFILE_CONFIG.

--- ext/exiv2/extconf.rb
+++ ext/exiv2/extconf.rb
@@ -1,9 +1,6 @@
 require 'mkmf'
 
-RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
-RbConfig::MAKEFILE_CONFIG['CCFLAGS'] = ENV['CCFLAGS'] if ENV['CCFLAGS']
-RbConfig::MAKEFILE_CONFIG['CXX'] = ENV['CXX'] if ENV['CXX']
-RbConfig::MAKEFILE_CONFIG['CXXFLAGS'] = ENV['CXXFLAGS'] if ENV['CXXFLAGS']
+$CXXFLAGS += " -std=c++11"
 RbConfig::CONFIG['PKG_CONFIG'] = 'pkg-config'
 
 if dir_config("exiv2") == [nil, nil]
