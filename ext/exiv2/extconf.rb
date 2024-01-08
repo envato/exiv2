237defca2484f5f1b3a37efb0ce02592a6cf79ef
Stanisław Pitucha <stan.pitucha@envato.com>
2024-01-08 16:14:50 +1100

    Include libssh and libcurl in compilation if needed

--- ext/exiv2/extconf.rb
+++ ext/exiv2/extconf.rb
@@ -7,4 +7,23 @@ if dir_config("exiv2") == [nil, nil]
   pkg_config("exiv2")
 end
 have_library("exiv2")
+
+# Some extensions are optional in versions <= 0.27 and also don't exist in
+# versions >= 0.28.
+# Check if they're enabled in the existing exiv2 headers
+# configuration and include the relevant libraries.
+if have_macro("EXV_USE_SSH", "exiv2/exv_conf.h")
+  if dir_config("libssh") == [nil, nil]
+    pkg_config("libssh")
+  end
+  have_library("libssh")
+end
+
+if have_macro("EXV_USE_CURL", "exiv2/exv_conf.h")
+  if dir_config("libcurl") == [nil, nil]
+    pkg_config("libcurl")
+  end
+  have_library("libcurl")
+end
+
 create_makefile("exiv2/exiv2")
